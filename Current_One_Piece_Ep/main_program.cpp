20f95b0a022c757edfe14595e766f9660b010351
Tomasz F <tomasz.f.2006@wp.pl>
2026-05-17 21:35:51 +0200

    initial commit

---
+++ Current_One_Piece_Ep/main_program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+//#include <cpprest/http_client.h>
+#include <windows.h>
+#include <wininet.h>
+#include <nlohmann/json.hpp>
+
+int main() {
+	return 0;
+	std::cout << "Test" << std::endl;
+}
